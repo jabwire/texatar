eca52d4d407ad08302f88e8bab9c8f899250289a
Darian Shimy <dshimy@gmail.com>
2013-01-19 19:36:19 -0800

    Readme, fonts, and better sizes.
    * Updated readme.md
    * Added font for better styling
    * Added colors based on text hash
    * Improved text size algorithm

--- app.rb
+++ app.rb
@@ -2,19 +2,46 @@ require 'rubygems'
 require 'sinatra'
 require 'RMagick'
 
+COLORS = [
+  ["#464646", "#e7e7e7"],
+  ["#b6cff5", "#0d3472"],
+  ["#98d7e4", "#0d3b44"],
+  ["#e3d7ff", "#3d188e"],
+  ["#fbd3e0", "#711a36"],
+  ["#f2b2a8", "#8a1c0a"],
+  ["#c2c2c2", "#ffffff"],
+  ["#4986e7", "#ffffff"],
+  ["#2da2bb", "#ffffff"],
+  ["#b99aff", "#ffffff"],
+  ["#f691b2", "#994a64"],
+  ["#fb4c2f", "#ffffff"],
+  ["#ffc8af", "#7a2e0b"],
+  ["#ffdeb5", "#824706"],
+  ["#fbe983", "#595a57"],
+  ["#fdedc1", "#714e07"],
+  ["#b3efd3", "#0b4f77"],
+  ["#a2dcc1", "#04506e"]
+  ]
+
 get '/' do
   erb :index
 end
 
 get '/image.png' do
+  message = params[:t]
+
+  result = 0
+  message.each_char { |c| result += c.ord }
+  result = result % COLORS.length
+  colors = COLORS[result]
+
   content_type 'image/png'
   size = params[:s].to_i || 48
   file = Magick::Image.new(size, size) do
-    self.background_color = "black"
+    self.background_color = colors[0]
   end
 
-  message = params[:t]
-  point_size = size
+  point_size = (0.8 * size)
   text = get_text(point_size)
   margin = size * 0.1
   iteration = 1
@@ -27,7 +54,7 @@ get '/image.png' do
   puts "  Iteration: #{iteration} Size: #{point_size} Width: #{text.get_type_metrics(file, message).width}"
 
   text.annotate(file, 0, 0, 0, 0, message) do
-    self.fill = '#ffffff'
+    self.fill = colors[1]
   end
 
   file.to_blob { |f| f.format = 'PNG' }
@@ -35,7 +62,7 @@ end
 
 def get_text(size)
   Magick::Draw.new do
-    self.font_family = 'helvetica neue, helvetica, arial'
+    self.font = 'fonts/Gafata-Regular.ttf'
     self.pointsize = size 
     self.gravity = Magick::CenterGravity
   end
--- readme.md
+++ readme.md
@@ -2,6 +2,27 @@
 
 A server that generates images with text to replace default gravatar image.
 
+The problem we face is not everyone has a gravatar image linkes to their email address.  When we show images in place of names, they all look the same and there is no way to differentiate between people.  Texatar will generate an image based on text for the user.  The color scheme is equally important and is based on a hash of the user's email.
+
+## Usage
+
+Images are generate from http://texatar.jabwire.com/avatar.png
+
+The following parameters used to control the generated image:
+
+* t = the text to display in the texatar, required
+* s = the size of the image in pixels, default 48
+
+## Gravatar Integration
+
+Gravatar allows developers to supply their own default image with the d= or default= parameter.  Be sure to URL encode the parameter's value.
+
+```
+<img src="http://www.gravatar.com/avatar/00000000000000000000000000000000?d=http%3A%2F%2Ftexatar.jabwire.com%2Favatar.png%3Fs%3D48%26t%3Dds"
+```
+
+See https://en.gravatar.com/site/implement/images/#default-image for more information.
+
 # Contact Information
 
 You can find me on Twitter: @dshimy or via gmail.
